fcfc2c4386687d625835a5351457270b58558d49
lamkr <lamkrodrigues@gmail.com>
2022-12-21 16:40:08 -0300

    Initial commit

---
+++ example/cucumber_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:cucumber_dart/cucumber_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/cucumber_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library cucumber_dart;
+
+export 'src/cucumber_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
